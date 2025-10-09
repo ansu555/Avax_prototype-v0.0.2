5e225257f5fcc1294565412e62d6296002eaa06c
ansu555 <anik200365@gmail.com>
2025-10-09 23:28:10 +0530

    Refactor connectWeb3 configuration to remove Base mainnet support, focusing on Avalanche and Fuji networks only for improved clarity.

--- app/services/connectWeb3.js
+++ app/services/connectWeb3.js
@@ -1,17 +1,16 @@
 // connectWeb3.ts
 import { createConfig, http } from 'wagmi';
-import { avalanche, avalancheFuji, base } from 'wagmi/chains';
+import { avalanche, avalancheFuji } from 'wagmi/chains';
 import { getDefaultConfig } from 'connectkit';
 
 export const wagmiConfig = createConfig(
   getDefaultConfig({
     appName: '10xSwap',
-  // Enable Avalanche mainnet, Avalanche Fuji (testnet), and Base mainnet
-  chains: [avalanche, avalancheFuji, base],
+  // Enable Avalanche mainnet and Avalanche Fuji (testnet) only
+  chains: [avalanche, avalancheFuji],
     transports: {
       [avalanche.id]: http(),
       [avalancheFuji.id]: http(),
-      [base.id]: http(),
     },
     autoConnect: false,
     // walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
